f2103fd51d1e39b7a078414712956e67fc32bc22
nyx-litenite <50533436+nyx-litenite@users.noreply.github.com>
2020-10-14 17:56:36 -0400

    embed message for paging

--- commands/leader.js
+++ commands/leader.js
@@ -7,6 +7,8 @@ module.exports = {
         const {JSDOM} = jsdom;
         const fetch = require('node-fetch');
         const querystring = require('querystring');
+        let Discord = require('discord.js');
+        const exampleEmbed = new Discord.MessageEmbed();
         let result = '';
         console.log(args[0])
         if (args[0] == 'page') {
@@ -15,25 +17,7 @@ module.exports = {
             }
             let final_url = `${url}?page=${args[1]}`;
             result = await fetch(`${url}?page=${args[1]}`).then(r => r.text());
-
-        }
-        else if (args[0] == 'name')
-        {
-            console.log('name')
-            if (!args[1] && !!args[1].length) {
-                return message.channel.send('You did not enter a username! Usernames are case-sensitive, so Litenite and litenite are not equal!');
-            }
-            console.log(args[1])
-            let body = {
-                search : args[1]
-            }
-            result = await fetch(url, {
-                method: "POST",
-                body: `search=${args[1]}`//JSON.stringify(body),
-              //  headers: { "Content-Type": "text/html" }
-            }).then(r => r.text());
-
-            console.log(result)
+            console.log(final_url)
         }
 
         const document = new JSDOM(result).window.document;
@@ -41,15 +25,16 @@ module.exports = {
         let data = Array.prototype.slice.call(document.querySelectorAll('tbody tr td')).map(f => f.textContent);
 
         let players = [];
-
+        if (data.length){
+            exampleEmbed.addField(`Leaderboard Page ${args[1]}`, 'XP               Kills     Deaths    KDR    HS      Streak     Rounds     Wins    Last Active', false);
+        }
         for (i = 0, j = 11; j < data.length + 11; i += 1, j+=11) 
         {
-            players.push(data.slice(i * 11, j).join(' '));
+            let player = data.slice(i * 11, j);
+            exampleEmbed.addField(`${player[0]}) ${player[1]}`, player.slice(2).join(' '), false);
         }
+    
 
-        //console.log({query : {url: final_url, result: players /*.forEach(f => f.textContent)*/}});
-
-
-        message.channel.send(players);
+        message.channel.send({embed: exampleEmbed});
 	},
 };
