245189eb64374749fc7d50d8bcbf3581778c144d
nyx-litenite <50533436+nyx-litenite@users.noreply.github.com>
2020-10-14 22:50:51 -0400

    abstract out the sendmessage logic and rename leader command to page

---
+++ bot.js
@@ -0,0 +1,25 @@
+class Bot {
+    constructor() {
+        this.data = '';
+    }
+    sendMessage(query, data, title) {
+        if (data.length){
+            let Discord = require('discord.js'); // necessary discord js require to get the EmbedMessage definition
+            const exampleEmbed = new Discord.MessageEmbed();
+
+            exampleEmbed.setDescription(title);
+            exampleEmbed.addField('Details', 'XP               Kills     Deaths    KDR    HS      Streak     Rounds     Wins    Last Active', false);
+
+            for (let i = 0, j = 11; j < data.length + 11; i += 1, j+=11) 
+            {
+                let player = data.slice(i * 11, j);
+                exampleEmbed.addField(`${player[0]}) ${player[1]}`, player.slice(2).join(' '), false);
+            }
+
+            return query.channel.send({embed: exampleEmbed});
+        }
+        query.channel.send("An error occurred. No Data was retrieved.")
+    }
+}
+
+module.exports = Bot;
---
+++ commands/page.js
@@ -0,0 +1,34 @@
+module.exports = {
+	name: 'page',
+	description: 'Get [count] players from the leaderboard',
+	async execute(message, args) {
+        let url = 'https://skillwarz.com/modern/leaderboard.php'; // url for the sw leaderboard (can be found using any browser's dev tools Network tab)
+        const jsdom = require('jsdom'); // node doesn't support dom natively, so import a dom parser 
+        const {JSDOM} = jsdom;
+        const fetch = require('node-fetch'); // not gonna lie just looked up node http requests. didn't see it had them native til later :/
+        const querystring = require('querystring'); // can be useful if you want to be careful w/ passing of parameters
+        let BOT  = require('../bot.js');
+
+        
+        let result = '';
+        let page = args[0];
+
+        if (!page && !Number.isNaN(page)) {
+            return message.channel.send('You did not enter page number! Currently, only pages 1-10 are available!');
+        }
+        if (page < 1)
+            page = 11 + Number(page); // allow referencing from the end (page 10 == page -1)
+            
+        result = await fetch(`${url}?page=${page}`).then(r => r.text());
+
+        const document = new JSDOM(result).window.document;
+
+        let data = Array.prototype.slice.call(document.querySelectorAll('tbody tr td')).map(f => f.textContent);
+
+        let players = [];
+
+        let bot = new BOT();
+
+        bot.sendMessage(message, data, `[Leaderboard Page ${page}](${url.replace('modern/', '')})`);
+	},
+};
